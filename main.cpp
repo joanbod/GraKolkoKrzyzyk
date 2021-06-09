97793577df3f27f280e24d7f1d4d8301807b0a87
joanbod <72557865+joanbod@users.noreply.github.com>
2021-06-09 09:38:09 +0200

    Mouse Buttons

--- main.cpp
+++ main.cpp
@@ -22,20 +22,23 @@ int main()
 	
 	//m1.how_to_play();
 
-	//Elementy w menu g³ównym
-	Sprites goBackButton = Sprites("images/start.png", 750, 50, true/*rozmiar 50X50*/);
+	//Elementy w menu gÂ³Ã³wnym
+	//Sprites goBackButton = Sprites("images/start.png", 750, 50, true/*rozmiar 50X50*/);
 	//Sprites m2 = Sprites("images/start.png", 350, 200, true);
 	//Sprites m3 = Sprites("images/start.png", 350, 300, true);
 
-	//£aduje czcionki
-	Texts o_x = Texts(L"O_X", "fonts/PressStart2P-Regular.ttf", 40,350,100);	//O_X na ka¿dej stronie
+	//Â£aduje czcionki
+	Texts o_x = Texts(L"O_X", "fonts/PressStart2P-Regular.ttf", 40,350,100);	//O_X na kaÂ¿dej stronie
 	Texts startButton = Texts(L"Start", "fonts/PressStart2P-Regular.ttf", 32, 350, 160);
 	Texts instructionButton = Texts(L"Instrukcja", "fonts/PressStart2P-Regular.ttf", 32, 350, 202);
-	wstring instruction = L"ZASADY GRY\nGracze obejmuj¹ pola na przemian d¹¿¹c do objêcia trzech pól w jednej linii,\nprzy jednoczesnym uniemo¿liwieniu tego samego przeciwnikowi.\nPole mo¿e byæ objête przez jednego gracza i\nnie zmienia swego w³aœciciela przez ca³y przebieg gry.\n\nJAK GRAÆ?\n Po pierwsze wybierz planszê:\n\n3x3\n5x5\n\n nastêpnie wybierz swojego przeciwnika:\n\n-£atwy\n-Œredni\n-Zaawansowany\n-Ze znajomym\n\nKoñcowo wybierz iloœæ rozgrywek wybieraj¹c odpowiedni¹ liczbê.\n\nI najwa¿niejsze pamiêtaj o dobrej zabawie.";
+	wstring instruction = L"ZASADY GRY\nGracze obejmujÂ¹ pola na przemian dÂ¹Â¿Â¹c do objÃªcia trzech pÃ³l w jednej linii,\nprzy jednoczesnym uniemoÂ¿liwieniu tego samego przeciwnikowi.\nPole moÂ¿e byÃ¦ objÃªte przez jednego gracza i\nnie zmienia swego wÂ³aÅ“ciciela przez caÂ³y przebieg gry.\n\nJAK GRAÃ†?\n Po pierwsze wybierz planszÃª:\n\n3x3\n5x5\n\n nastÃªpnie wybierz swojego przeciwnika:\n\n-Â£atwy\n-Å’redni\n-Zaawansowany\n-Ze znajomym\n\nKoÃ±cowo wybierz iloÅ“Ã¦ rozgrywek wybierajÂ¹c odpowiedniÂ¹ liczbÃª.\n\nI najwaÂ¿niejsze pamiÃªtaj o dobrej zabawie.";
 	Texts instructionText = Texts(instruction, "fonts/Quicksand-VariableFont_wght.ttf", 16, 100, 160);
 	Texts chooseGameBoard = Texts(L"PLANSZA", "fonts/PressStart2P-Regular.ttf", 22, 350, 160);
 	Texts chooseGameMode = Texts(L"TRYB GRY", "fonts/PressStart2P-Regular.ttf", 22, 350, 192);
 	Texts chooseAmountOfGames = Texts(L"LICZBA ROZGRYWEK", "fonts/PressStart2P-Regular.ttf", 22, 350, 234);
+	Texts threeXthree = Texts(L"3 x 3", "fonts/PressStart2P-Regular.ttf", 32, 360, 250);
+	Texts fiveXfive = Texts(L"5 x 5", "fonts/PressStart2P-Regular.ttf", 32, 360, 310);
+	Texts goBackButton = Texts(L"BACK", "fonts/PressStart2P-Regular.ttf", 14, 740, 20);
 	
 	sf::RenderWindow window(sf::VideoMode(800, 650, 32), "Pierwsze okno"/*, sf::Style::Fullscreen*/);
 	window.setVerticalSyncEnabled(true);
@@ -50,73 +53,77 @@ int main()
 		sf::Event e;
 		while (window.pollEvent(e))
 		{
+			
 			//obsluga zdarzenia
 			if (e.type == sf::Event::Closed || (e.type == sf::Event::KeyPressed && e.key.code == sf::Keyboard::Escape))
 			{
 				window.close();
 			}
-			//Powrót do strony g³ównej
-			if (!(mod == 0) && e.type == sf::Event::MouseButtonReleased && e.mouseButton.button == sf::Mouse::Right && e.mouseButton.x < 800 && e.mouseButton.x > 0 && e.mouseButton.y > 0 && e.mouseButton.y < 650)
+			//PowrÃ³t do strony gÅ‚Ã³wnej
+			if (!(mod == 0) && e.type == sf::Event::MouseButtonReleased && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 800 && e.mouseButton.x > 740 && e.mouseButton.y > 0 && e.mouseButton.y < 40)
 			{
 				mod = 0;
+				choiceMode = 1;
+				//TODO: moÅ¼na tu zrobiÄ‡ powrÃ³t do poprzedniej storny mod--
 			}
-			if (mod == 0 && e.type == sf::Event::MouseButtonPressed && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 450 && e.mouseButton.x > 350 && e.mouseButton.y > 202 && e.mouseButton.y < 234)
+			if (mod == 0 && e.type == sf::Event::MouseButtonReleased && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 640 && e.mouseButton.x > 210 && e.mouseButton.y > 320 && e.mouseButton.y < 364)
 			{
-				//Przejœcie do instrukcji.
+				//PrzejÅ›cie do instrukcji.
 				mod = 1;
 			}
-			if (mod == 0 && e.type == sf::Event::MouseButtonPressed && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 450 && e.mouseButton.x > 350 && e.mouseButton.y > 160 && e.mouseButton.y < 192)
+			else if (mod == 0 && e.type == sf::Event::MouseButtonReleased && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 510 && e.mouseButton.x > 290 && e.mouseButton.y < 300 && e.mouseButton.y > 230)
 			{
-				//Przejœcie do wyboru trybu gry.
+				//PrzejÅ›cie do wyboru trybu gry.
 				mod = 2;
 			}
-			//wybór trybu gr 3x3 albo 5x5
-			if (mod == 2 && choiceMode == 1 && e.type == sf::Event::MouseButtonPressed && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 400 && e.mouseButton.x > 0 && e.mouseButton.y > 0 && e.mouseButton.y < 650)
+			//wybÃ³r trybu gr 3x3 albo 5x5
+			else if (mod == 2 && choiceMode == 1 && e.type == sf::Event::MouseButtonReleased && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 440 && e.mouseButton.x > 360 && e.mouseButton.y > 248 && e.mouseButton.y < 285)
 			{
 				vecChoices.push_back(static_cast<int>(GameBoard::threexthree));
 				choiceMode = 2;
 			}
-			if (mod == 2 && choiceMode == 1 && e.type == sf::Event::MouseButtonPressed && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 800 && e.mouseButton.x > 400 && e.mouseButton.y > 0 && e.mouseButton.y < 650)
+			else if (mod == 2 && choiceMode == 1 && e.type == sf::Event::MouseButtonReleased && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 800 && e.mouseButton.x > 400 && e.mouseButton.y > 300 && e.mouseButton.y < 650)
 			{
-				vecChoices.push_back(static_cast<int>(GameBoard::threexthree));
+				vecChoices.push_back(static_cast<int>(GameBoard::fivexfive));
 				choiceMode = 2;
 			}
-			//wybor poziomu trudnoœci lub czy z innym graczem 
-			if (mod == 2 && choiceMode == 2 && e.type == sf::Event::MouseButtonPressed && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 400 && e.mouseButton.x > 0 && e.mouseButton.y < 300 && e.mouseButton.y > 0)
+			//wybor poziomu trudnoÅ›ci lub czy z innym graczem 
+			else if (mod == 2 && choiceMode == 2 && e.type == sf::Event::MouseButtonReleased && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 400 && e.mouseButton.x > 0 && e.mouseButton.y < 300 && e.mouseButton.y > 0)
 			{
 				vecChoices.push_back(static_cast<int>(GameMode::easy));
 				choiceMode = 3;
 			}
-			if (mod == 2 && choiceMode == 2 && e.type == sf::Event::MouseButtonPressed && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 400 && e.mouseButton.x > 0 && e.mouseButton.y < 650 && e.mouseButton.y > 300)
+			else if (mod == 2 && choiceMode == 2 && e.type == sf::Event::MouseButtonReleased && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 400 && e.mouseButton.x > 0 && e.mouseButton.y < 650 && e.mouseButton.y > 300)
 			{
 				vecChoices.push_back(static_cast<int>(GameMode::advanced));
 				choiceMode = 3;
 			}
-			if (mod == 2 && choiceMode == 2 && e.type == sf::Event::MouseButtonPressed && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 800 && e.mouseButton.x > 400 && e.mouseButton.y < 300 && e.mouseButton.y > 0)
+			else if (mod == 2 && choiceMode == 2 && e.type == sf::Event::MouseButtonReleased && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 800 && e.mouseButton.x > 400 && e.mouseButton.y < 300 && e.mouseButton.y > 0)
 			{
 				vecChoices.push_back(static_cast<int>(GameMode::medium));
 				choiceMode = 3;
 			}
-			if (mod == 2 && choiceMode == 2 && e.type == sf::Event::MouseButtonPressed && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 800 && e.mouseButton.x > 400 && e.mouseButton.y < 650 && e.mouseButton.y > 300)
+			else if (mod == 2 && choiceMode == 2 && e.type == sf::Event::MouseButtonReleased && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 800 && e.mouseButton.x > 400 && e.mouseButton.y < 650 && e.mouseButton.y > 300)
 			{
 				vecChoices.push_back(static_cast<int>(GameMode::withAFriend));
 				choiceMode = 3;
 			}
-			if (mod == 2 && choiceMode == 3 && e.type == sf::Event::MouseButtonPressed && e.mouseButton.button == sf::Mouse::Left)
+			else if (mod == 2 && choiceMode == 3 && e.type == sf::Event::MouseButtonReleased && e.mouseButton.button == sf::Mouse::Left && e.mouseButton.x < 800 && e.mouseButton.x > 400 && e.mouseButton.y < 650 && e.mouseButton.y > 300)
 			{
-				choiceMode = 1;
+				//tutaj chhyba inputem jakims bedzie trzeba to zrobic
+				mod = 3;
 			}
 
-			//TODO: przycisk powrotu, mo¿e ma³e menu po najechaniu siê wysietla? przyciski wyboru gry bo rozpierdala to powoli.
+			//TODO: przycisk powrotu, moÂ¿e maÂ³e menu po najechaniu siÃª wysietla? przyciski wyboru gry bo rozpierdala to powoli.
 		}
 
 
 		window.clear(sf::Color(0, 0, 0));
-		//TODO: Do osobnej funkjci to strzeliæ by trzeba by³o.
+		//TODO: Do osobnej funkjci to strzeliÃ¦ by trzeba byÂ³o.
 	
 		switch (mod)
 		{
-			//G³ówne menu gry
+			//GÂ³Ã³wne menu gry
 		case 0:
 			window.draw(o_x.text);
 			window.draw(startButton.text);
@@ -126,22 +133,28 @@ int main()
 		case 1:
 			window.draw(o_x.text);
 			window.draw(instructionText.text);
-			window.draw(goBackButton.sprite);
+			window.draw(goBackButton.text);
 
 			break;
-			//Wybór trybu gry
+			//WybÃ³r trybu gry
 		case 2:
 		{
 			switch (choiceMode)
 			{
 			case 1:
 				window.draw(chooseGameBoard.text);
+				window.draw(threeXthree.text);
+				window.draw(fiveXfive.text);
+				window.draw(goBackButton.text);
 				break;
 			case 2:
 				window.draw(chooseGameMode.text);
+				
+				window.draw(goBackButton.text);
 				break;
 			case 3:
 				window.draw(chooseAmountOfGames.text);
+				window.draw(goBackButton.text);
 				break;
 
 			}
