3e4ddf7d88eda93925f3ed2a4bcf4ce2afa29d2d
Wojciech Brożek <wobrozek@gmail.com>
2021-06-09 10:25:02 +0200

    board center

--- board.cpp
+++ board.cpp
@@ -1,23 +1,24 @@
 #include "board.h"
 
-board::board(RenderWindow *okno, int a) {
+board::board(RenderWindow* okno, int a) {
 	rozmiar = a;
-	window= okno;
+	window = okno;
 	int liczba_pol = rozmiar * rozmiar;
-	tab= new RectangleShape[liczba_pol];
+	tab = new RectangleShape[liczba_pol];
+	tab_sore = new int[liczba_pol];
 
 }
 
 void board::rysuj() {
-	int x = 1, y = 3;
-	for (int i = 0; i < rozmiar*rozmiar; i++)
+	int x = 400 - (rozmiar * 80), y = 3;
+	for (int i = 0; i < rozmiar * rozmiar; i++)
 	{
 		RectangleShape square(Vector2f(80.f, 80.f));
 		tab[i] = square;
 		tab[i].setFillColor(Color(0, 0, 0));
 		if (i % rozmiar == 0) {
 			y += 80;
-			x = 1;
+			x = 400 - (rozmiar / 2 * 80);
 		}
 		tab[i].move(x, y);
 		x += 80;
@@ -25,7 +26,11 @@ void board::rysuj() {
 		tab[i].setOutlineColor(Color(255, 255, 255));
 		window->draw(tab[i]);
 	}
-	
+
+}
+
+void board::wstaw() {
+
 }
 
 board::~board() {
--- board.h
+++ board.h
@@ -12,13 +12,15 @@ class board
 public:
 	board(RenderWindow* window, int rozmiar);
 	void rysuj();
+	void wstaw(int x,int y,int kto);   // 1-kolko    2-krzyzyk
 	~board();
 
 private:
 	RenderWindow* window;
 	int rozmiar;
 	RectangleShape* tab;
-	
+	int* tab_score[];
+
 };
 
 
