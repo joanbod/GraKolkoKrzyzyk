2ccc5795870c22a24cd73e87867dea38f67d645e
joanbod <72557865+joanbod@users.noreply.github.com>
2021-06-16 22:03:40 +0200

    Only Bot class

--- bot.h
+++ bot.h
@@ -13,61 +13,19 @@ class Board;
 
 class Bot
 {
+	int tryb_gry;//pobiera wartość wektora z indesku 1 (1-3)
+	Board* plansza;
+
 public:
-	virtual bool czy_pole_puste(int i) = 0;
-	virtual int sprawdz_czy_wygram() = 0;
-	virtual int sprawdz_czy_zablokuje() = 0;
-	virtual void dodaj_losowo_wartosc() = 0;
-	virtual int dodaj_losowo_wartosc_optymalniej() = 0;
-	virtual void wykonaj_ruch() = 0;
+	Bot(Board&, int);
+	virtual ~Bot();
+
+	bool czy_pole_puste(int i) ;
+	int sprawdz_czy_wygram() ;
+	int sprawdz_czy_zablokuje();
+	void dodaj_losowo_wartosc() ;
+	int dodaj_losowo_wartosc_optymalniej() ;
+	void wykonaj_ruch();
 
 	friend int losowa_liczba(int a,int b);
-};
-
-class Easy :public Bot
-{
-	Board* plansza;
-
-public:
-	Easy(Board&);
-	virtual ~Easy();
-
-	bool czy_pole_puste(int i);
-	void dodaj_losowo_wartosc();
-	
-
-	
-};
-
-class Medium :public Bot
-{
-	Board* plansza;
-	
-public:
-	Medium(Board&);
-	virtual ~Medium();
-
-	bool czy_pole_puste(int i);
-	int sprawdz_czy_wygram();
-	int dodaj_losowo_wartosc_optymalniej();
-
-	void wykonaj_ruch();
-	
-};
-
-class Advanced :public Bot
-{
-	Board* plansza;
-	
-
-public:
-	Advanced(Board&);
-	virtual ~Advanced();
-
-	bool czy_pole_puste(int i);
-	int sprawdz_czy_wygram();
-	int sprawdz_czy_zablokuje();
-	int dodaj_losowo_wartosc_optymalniej();
-	
-	void wykonaj_ruch();
 };
